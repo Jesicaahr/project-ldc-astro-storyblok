625fcd48414a0e16f4f0c68e9a3b14054cee5e56
iyyafi <iyyafi@gmail.com>
2024-06-27 10:33:44 +0700

    update footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#2897ec',
-				'primary-darker': '#1d519a',
-			},
-			fontFamily: {
-				'sans': ['Work Sans', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2897ec",
+        "primary-darker": "#1d519a",
+        secondary: "#f8a833",
+      },
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        display: ["Raleway", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
