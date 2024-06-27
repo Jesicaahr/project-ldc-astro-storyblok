c33de4276d1b60018ba9bab5a0b16b51dc96fabf
iyyafi <iyyafi@gmail.com>
2024-06-27 09:01:40 +0700

    update footer copy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#2897ec',
+				'primary-darker': '#1d519a',
+			},
+			fontFamily: {
+				'sans': ['Work Sans', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
