d9393f125e89d51f7ab4eab4a9dd93f94ced5516
iyyafi <iyyafi@gmail.com>
2024-06-28 20:30:36 +0700

    fix footer responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,14 +5,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        xl: "1200px",
+        "2xl": "1200px",
+      },
+    },
     extend: {
       colors: {
         primary: "#2897ec",
         "primary-darker": "#1d519a",
         secondary: "#f8a833",
-      "text-primary": "#231f20",
-      "text-secondary": "#9b9b9b",
-
+        "text-primary": "#231f20",
+        "text-secondary": "#9b9b9b",
       },
       fontFamily: {
         sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
